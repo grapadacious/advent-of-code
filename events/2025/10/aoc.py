216c1bae6c8424b1529588017b287d367adefe42
Patrick McDonald <patrick.m.mcdonald@icloud.com>
2025-12-10 23:50:09 -0500

    WIP - Part 1 working

---
+++ events/2025/10/aoc.py
@@ -0,0 +1,65 @@
+class Specification:
+    def __init__(self, line):
+        segments = line.split(" ")
+        self.lights = frozenset([i - 1 for i in range(len(segments[0])) if segments[0][i] == "#"])
+        self.buttons = [frozenset([int(n) for n in s[1:len(s) - 1].split(",")]) for s in segments[1:len(segments) - 1]]
+        self.joltages = [int(n) for n in segments[-1][1:len(segments[-1]) - 1].split(",")]
+        self.matrix = [[1 if i in b else 0 for i in range(len(self.joltages))] for b in self.buttons]
+
+    def fewest_presses_for_lights(self):
+        states = set(b for b in self.buttons)
+
+        c = 1
+        while self.lights not in states:
+            updated_states = set()
+
+            for state in states:
+                for b in self.buttons:
+                    updated_states.add(state.symmetric_difference(b))
+
+            states = updated_states
+            c += 1
+
+        return c
+    
+    def fewest_presses_for_joltages(self, joltages):
+        if joltages == self.zero_joltages:
+            return 0
+        
+        if -1 in set(joltages):
+            return -1
+        
+        result = sum(self.joltages) + 1
+        for button in self.buttons:
+            new_result = self.fewest_presses_for_joltages(tuple([joltages[i] - 1 if i in button else joltages[i] for i in range(len(joltages))]))
+
+            if new_result == -1:
+                continue
+
+            result = min(result, new_result)
+            
+        return result + 1
+
+    def __repr__(self):
+        r = f"Lights: {set(l for l in self.lights)}\nButtons: {[set(l for l in b) for b in self.buttons]}\nJoltages: {self.joltages}\nMatrix:\n"
+        r += "\n".join([f"  {row}" for row in self.matrix])
+        return r
+
+def part_one(input: list[str]):
+    specifications = [Specification(line) for line in input]
+
+    total = 0
+    for spec in specifications:
+        total += spec.fewest_presses_for_lights()
+
+    return total
+
+def part_two(input: list[str]):
+    specifications = [Specification(line) for line in input]
+
+    total = 0
+    for spec in specifications:
+        print(spec)
+        print()
+
+    return total
